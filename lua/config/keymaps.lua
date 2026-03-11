e5def428b15db32ef96e6cadadaf3e0e69ca902c
Paulo Chaves <opaulochaves@gmail.com>
2026-03-11 15:35:56 -0300

    feat(keymaps): add buffer management shortcuts
    Add leader mappings for next/previous buffer and buffer cleanup actions: close current, close others, and close all buffers.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,19 @@ map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit window" })
 map("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
 
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map("n", "<leader>bo", function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.bo[buf].buflisted then
+      vim.cmd("bdelete " .. buf)
+    end
+  end
+end, { desc = "Delete other buffers" })
+map("n", "<leader>ba", "<cmd>%bdelete|edit#|bdelete#<cr>", { desc = "Delete all buffers" })
+
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
 map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
